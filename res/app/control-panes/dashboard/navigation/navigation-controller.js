a232fa5d74b9671a779ac6386e9c4e86cb2b6ae5
song <thinkhy@qq.com>
2017-05-21 00:34:32 +0800

    test template view works

--- res/app/control-panes/dashboard/navigation/navigation-controller.js
+++ res/app/control-panes/dashboard/navigation/navigation-controller.js
@@ -2,76 +2,76 @@ var _ = require('lodash')
 
 module.exports = function NavigationCtrl($scope, $rootScope) {
 
-  var faviconIsSet = false
+    var faviconIsSet = false
 
-  function setUrlFavicon(url) {
-    var FAVICON_BASE_URL = '//www.google.com/s2/favicons?domain_url='
-    $scope.urlFavicon = FAVICON_BASE_URL + url
-    faviconIsSet = true
-  }
-
-  function resetFavicon() {
-    $scope.urlFavicon = require('./default-favicon.png')
-    faviconIsSet = false
-  }
-
-  resetFavicon()
-
-  $scope.textUrlChanged = function() {
-    if (faviconIsSet) {
-      resetFavicon()
+    function setUrlFavicon(url) {
+      var FAVICON_BASE_URL = '//www.google.com/s2/favicons?domain_url='
+      $scope.urlFavicon = FAVICON_BASE_URL + url
+      faviconIsSet = true
     }
-  }
 
-  function addHttp(textUrl) {
-    // Check for '://' because a protocol-less URL might include
-    // a username:password combination.
-    // Ignores also any query parameter because it may contain a http:// inside.
-    return (textUrl.replace(/\?.*/, '').indexOf('://') === -1 ? 'http://' : ''
-      ) + textUrl
-  }
+    function resetFavicon() {
+      $scope.urlFavicon = require('./default-favicon.png')
+      faviconIsSet = false
+    }
 
-  $scope.blurUrl = false
+    resetFavicon()
 
-  $scope.openURL = function() {
-    $scope.blurUrl = true
-    $rootScope.screenFocus = true
-
-    var url = addHttp($scope.textURL)
-    setUrlFavicon(url)
-    return $scope.control.openBrowser(url, $scope.browser)
-  }
-
-  function setCurrentBrowser(browser) {
-    if (browser && browser.apps) {
-      var currentBrowser = {}
-      if (browser.selected) {
-        var selectedBrowser = _.head(browser.apps, 'selected')
-        if (!_.isEmpty(selectedBrowser)) {
-          currentBrowser = selectedBrowser
-        }
-      } else {
-        var defaultBrowser = _.find(browser.apps, {name: 'Browser'})
-        if (defaultBrowser) {
-          currentBrowser = defaultBrowser
-        } else {
-          currentBrowser = _.head(browser.apps)
-        }
+    $scope.textUrlChanged = function() {
+      if (faviconIsSet) {
+        resetFavicon()
       }
-      $rootScope.browser = currentBrowser
     }
-  }
 
-  setCurrentBrowser($scope.device ? $scope.device.browser : null)
-
-  $scope.$watch('device.browser', function(newValue, oldValue) {
-    if (newValue !== oldValue) {
-      setCurrentBrowser(newValue)
+    function addHttp(textUrl) {
+      // Check for '://' because a protocol-less URL might include
+      // a username:password combination.
+      // Ignores also any query parameter because it may contain a http:// inside.
+      return (textUrl.replace(/\?.*/, '').indexOf('://') === -1 ? 'http://' : ''
+        ) + textUrl
     }
-  }, true)
 
-  $scope.clearSettings = function() {
-    var browser = $scope.browser
-    $scope.control.clearBrowser(browser)
-  }
+    $scope.blurUrl = false
+
+    $scope.openURL = function() {
+      $scope.blurUrl = true
+      $rootScope.screenFocus = true
+
+      var url = addHttp($scope.textURL)
+      setUrlFavicon(url)
+      return $scope.control.openBrowser(url, $scope.browser)
+    }
+
+    function setCurrentBrowser(browser) {
+      if (browser && browser.apps) {
+        var currentBrowser = {}
+        if (browser.selected) {
+          var selectedBrowser = _.head(browser.apps, 'selected')
+          if (!_.isEmpty(selectedBrowser)) {
+            currentBrowser = selectedBrowser
+          }
+        } else {
+          var defaultBrowser = _.find(browser.apps, {name: 'Browser'})
+          if (defaultBrowser) {
+            currentBrowser = defaultBrowser
+          } else {
+            currentBrowser = _.head(browser.apps)
+          }
+        }
+        $rootScope.browser = currentBrowser
+      }
+    }
+
+    setCurrentBrowser($scope.device ? $scope.device.browser : null)
+
+    $scope.$watch('device.browser', function(newValue, oldValue) {
+      if (newValue !== oldValue) {
+        setCurrentBrowser(newValue)
+      }
+    }, true)
+
+    $scope.clearSettings = function() {
+      var browser = $scope.browser
+      $scope.control.clearBrowser(browser)
+    }
 }
