1f6cfc3b272368f9bb0211b1e3aca950a34f9d0c
thinkhy <thinkhy@gmail.com>
2017-06-02 20:52:09 +0800

    determine if logcat field is undefined

--- res/app/testing/commands/commands-controller.js
+++ res/app/testing/commands/commands-controller.js
@@ -166,7 +166,7 @@ module.exports = function CommandsCtrl(
     var testID = data['id']
     var status = data['status']
     var end = data['endTime']
-    var logcat = data['logcat']
+    var logcat = data['logcat']||""
     // $scope.columns.forEach(function(obj){
     for (var i = 0; i < $scope.columns.length; i++) {
       obj = $scope.columns[i]
