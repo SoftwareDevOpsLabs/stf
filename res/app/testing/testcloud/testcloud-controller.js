2a39dcfb2e4c5981049d6b1b7e516b72147d8374
song <thinkhy@qq.com>
2017-06-27 22:13:18 +0800

    debug

--- res/app/testing/testcloud/testcloud-controller.js
+++ res/app/testing/testcloud/testcloud-controller.js
@@ -8,13 +8,13 @@ module.exports = function TestcloudCtrl(
   $http
 ) {
      $scope.clouds = [
-       {name: "开源", URL: "http://opentest.360.cn/index.html", item: "360"},
-       {name: "OpenLab", URL: "https://deveco.huawei.com/manualinstuction", item: "华为"},
-       {name: "Testin", URL: "http://www.testin.cn/", item: "云测"},
-       {name: "MTC", URL: "http://mtc.baidu.com/", item: "百度" },
-       {name: "优测", URL: "http://mtc.baidu.com/", item: "腾讯" },
-       {name: "MQC", URL: "http://mqc.aliyun.com/", item: "阿里巴巴" },
-       {name: "易测云", URL: "http://www.yiceyun.com/", item: "易测云" },
+       {name: "开源", URL: "http://opentest.360.cn/index.html", company: "360"},
+       {name: "OpenLab", URL: "https://deveco.huawei.com/manualinstuction", company: "华为"},
+       {name: "Testin", URL: "http://www.testin.cn/", company: "云测"},
+       {name: "MTC", URL: "http://mtc.baidu.com/", company: "百度" },
+       {name: "优测", URL: "http://mtc.baidu.com/", company: "腾讯" },
+       {name: "MQC", URL: "http://mqc.aliyun.com/", company: "阿里巴巴" },
+       {name: "易测云", URL: "http://www.yiceyun.com/", company: "易测云" },
      ]
     // $scope.$apply()
      console.log($scope.clouds)
