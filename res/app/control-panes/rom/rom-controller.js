7a9a3769c04f14cff49a16536d7b134384a96591
Hao8816 <chenhao8816+8816@gmail.com>
2017-07-12 00:25:18 +0800

    修改rom列表显示

--- res/app/control-panes/rom/rom-controller.js
+++ res/app/control-panes/rom/rom-controller.js
@@ -1,18 +1,27 @@
-module.exports = function RomCtrl($scope,
-                                  $http) {
+module.exports = function RomCtrl(
+  $scope,
+  $http,
+  $filter
+) {
   console.log("+++++++++++++++++ Rom Ctrl +++++++++++++++")
 
   $scope.rom = {
     romlist: [],
   }
 
+  // 是否显示日志
+  $scope.show_win = true
+
   $scope.logs = []
-  // @chenhao log 的数据格式
-  // {'time': '2017-07-10 12:22:51','msg':'程序安装完毕。'}
 
   $scope.refreshRom = function refreshRom(rom) {
      var command = "am instrument -w -r -e debug false -e class com.qihoo.caes.FlashPhone#testOTAUpgrade"
                      + " -e Loop 1 -e nickname Pro6 " + " -e url " + rom.ftpDownload + " com.qihoo.test/android.support.test.runner.AndroidJUnitRunner&"
+
+      // 初始化刷机消息
+      var time = $filter('date')(new Date(),'yyyy-MM-dd HH:mm:ss')
+      $scope.logs.push({'msg':command,'time':time})
+      $scope.show_win = true
 
       console.log(command)
       var oneHour = 3600*1000  // timeout for rom refresh is one hour
@@ -20,15 +29,25 @@ module.exports = function RomCtrl($scope,
       .progressed(function(result) {
         console.log(result)
           //@hy TODO 数据格式和上面定义的$scope.logs 一致
-        $scope.result = result
-        $scope.logs.push(result)
+        var log = result['lastData']
+        var time = $filter('date')(new Date(),'yyyy-MM-dd HH:mm:ss')
+        if (log){
+          $scope.logs.push({'msg':log,'time':time})
+        }
       })
        .then(function(result) {
          console.log(result)
-          //@hy TODO 数据格式和上面定义的$scope.logs 一致
-         $scope.result = result
-         $scope.logs.push(result)
+        var log = result['lastData']
+        var time = $filter('date')(new Date(),'yyyy-MM-dd HH:mm:ss')
+        if (log){
+          $scope.logs.push({'msg':log,'time':time})
+        }
       })
+  }
+
+  // 隐藏刷机窗口
+  $scope.hideLogWin = function () {
+    $scope.show_win = false
   }
 
   // Get rom list for specific model
